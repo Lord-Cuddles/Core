f0037dc031cb4d4394f5060f720c7a154ce4b28c
Lord-Cuddles <georgeburton666@hotmail.com>
2020-06-11 16:44:43 +0100

    Create startup.lua

---
+++ startup.lua
@@ -0,0 +1,25 @@
+local version = "1.0 alpha 1"
+
+if not fs.exists("update.lua") then
+  -- Download 'update.lua' from GitHub
+    local site = http.get(
+        "https://raw.githubusercontent.com/Lord-Cuddles/Core/master/update.lua"
+    )
+  
+    if not site then
+        -- Failed to connect to GitHub
+        return error("Unable to connect to GitHub. Please try again later")
+    end
+  
+    local content = site.readAll()
+    site.close()
+    local file = fs.open("update.lua", "w")
+    file.write(content)
+    file.close()
+end
+
+if fs.exists("main.lua") then
+    shell.run("main.lua")
+else
+    shell.run("update.lua")
+end
